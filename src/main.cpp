3e79aba6273afc9c206e71173f19428a655cbf0b
teuthid <mariusz.przygodzki@gmail.com>
2018-12-01 22:12:12 +0100

    upload to master

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 #ifdef EQ_DEBUG
 #define _TASK_DEBUG
-#endif // EQ_DEBUG
+#endif
 
 #include "eq_tasks.h"
 #include <TaskScheduler.h>
@@ -49,26 +49,26 @@ bool eqInit() {
 void printConfig() {
   Serial.println();
   Serial.println(F("Configuration:"));
-  Serial.print(F("- EEPROM: "));
+  Serial.print(F(" EEPROM: "));
   for (uint16_t __i = EqEeprom::startAddress;
        __i <= EqEeprom::startAddress + 50; __i++) {
     Serial.print(EEPROM.read(__i), HEX);
     Serial.print(F(" "));
   }
   Serial.println();
-  Serial.print(F("- Light Sensor Threshold [%] = "));
+  Serial.print(F(" Light Sensor Threshold = "));
   Serial.println(EqConfig::lightSensorThreshold());
-  Serial.print(F("- HT Sensor Interval [s] = "));
+  Serial.print(F(" HT Sensor Interval = "));
   Serial.println(EqConfig::htSensorInterval());
-  Serial.print(F("- HT Sensor Temperature Threshold [*C] = "));
+  Serial.print(F(" HT Sensor Temperature Threshold = "));
   Serial.println(EqConfig::htSensorTemperatureThreshold(), 1);
-  Serial.print(F("- HT Sensor Humidity Threshold [%] = "));
+  Serial.print(F(" HT Sensor Humidity Threshold = "));
   Serial.println(EqConfig::htSensorHumidityThreshold());
-  Serial.print(F("- HT Index Type = "));
+  Serial.print(F(" HT Index Type = "));
   Serial.println(static_cast<uint8_t>(EqConfig::htIndexType()));
-  Serial.print(F("- Overdrive Step [s] = "));
+  Serial.print(F(" Overdrive Step = "));
   Serial.println(EqConfig::overdriveStep());
-  Serial.print(F("- Fan Tachometer = "));
+  Serial.print(F(" Fan Tachometer = "));
   Serial.println(EqConfig::isFanTachometerEnabled() ? F("Enabled")
                                                     : F("Disabled"));
   Serial.println(F("Running..."));
