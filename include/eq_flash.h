0f435b7a758dd39f3a125754702d97b192254622
teuthid <mariusz.przygodzki@gmail.com>
2019-03-05 23:56:35 +0100

    upload to master

--- include/eq_flash.h
+++ include/eq_flash.h
@@ -14,20 +14,23 @@
 
 class EqFlashString : public Printable {
 public:
-  EqFlashString(const void *pstr) {
-    pstr_ = reinterpret_cast<const char *>(pstr);
-  }
+  constexpr EqFlashString(const void *pstr)
+      : pstr_(reinterpret_cast<const char *>(pstr)) {}
+  
   size_t length() const { return strlen_P(pstr_); }
+  
   int compare(const char *str) const { return -strcmp_P(str, pstr_); }
   int compare_n(const char *str, size_t n) const {
     return -strncmp_P(str, pstr_, n);
   }
+  
   char *copy(char *str, size_t offset = 0) const {
     return strcpy_P(str, pstr_ + offset);
   }
   char *copy_n(char *str, size_t n, size_t offset = 0) const {
     return strncpy_P(str, pstr_ + offset, n);
   }
+  
   char *append(char *str) const { return strcat_P(str, pstr_); }
   char *append_n(char *str, size_t n) const { return strncat_P(str, pstr_, n); }
 
