45b507742a41e3bc5d56ca8f4f031c4da6cbd14e
teuthid <mariusz.przygodzki@gmail.com>
2019-03-06 21:14:57 +0100

    upload to master

--- include/eq_flash.h
+++ include/eq_flash.h
@@ -8,36 +8,29 @@
 
 #include "Arduino.h"
 
-#ifdef EQ_ARCH_AVR
+#if defined(EQ_ARCH_AVR)
 #include <avr/pgmspace.h>
 
 class EqFlashString : public Printable {
 public:
   constexpr EqFlashString(const char *pstr) : pstr_(pstr) {}
-
   size_t length() const { return strlen_P(pstr_); }
-
   int compare(const char *str) const { return -strcmp_P(str, pstr_); }
   int compare_n(const char *str, size_t n) const {
     return -strncmp_P(str, pstr_, n);
   }
-
   char *copy(char *str, size_t offset = 0) const {
     return strcpy_P(str, pstr_ + offset);
   }
   char *copy_n(char *str, size_t n, size_t offset = 0) const {
     return strncpy_P(str, pstr_ + offset, n);
   }
-
   char *append(char *str) const { return strcat_P(str, pstr_); }
   char *append_n(char *str, size_t n) const { return strncat_P(str, pstr_, n); }
-
   char operator[](size_t index) const {
     return static_cast<char>(pgm_read_byte(pstr_ + index));
   }
-
   char *operator()(char *str) const { return strcpy_P(str, pstr_); }
-
   size_t printTo(Print &p) const { // virtual method from Printable class
     return p.print((__FlashStringHelper *)pstr_);
   }
@@ -51,7 +44,35 @@ private:
   EqFlashString name(__flash_##name);
 
 #else // without using flash memory
-// TODO
+
+class EqFlashString : public Printable {
+public:
+  constexpr EqFlashString(const char *pstr) : pstr_(pstr) {}
+  size_t length() const { return strlen(pstr_); }
+  int compare(const char *str) const { return -strcmp(str, pstr_); }
+  int compare_n(const char *str, size_t n) const {
+    return -strncmp(str, pstr_, n);
+  }
+  char *copy(char *str, size_t offset = 0) const {
+    return strcpy(str, pstr_ + offset);
+  }
+  char *copy_n(char *str, size_t n, size_t offset = 0) const {
+    return strncpy(str, pstr_ + offset, n);
+  }
+  char *append(char *str) const { return strcat(str, pstr_); }
+  char *append_n(char *str, size_t n) const { return strncat(str, pstr_, n); }
+  char operator[](size_t index) const { return pstr_[index]; }
+  char *operator()(char *str) const { return strcpy(str, pstr_); }
+  size_t printTo(Print &p) const { return p.print(pstr_); }
+
+private:
+  const char *pstr_;
+};
+
+#define EQ_FSTR(name, value)                                                   \
+  static const char __flash_##name[] = value;                                  \
+  EqFlashString name(__flash_##name);
+
 #endif
 
 #endif // __EQ_FLASH_H__
