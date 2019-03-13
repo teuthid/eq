ffb407f33777e7a8ee424377d368f45e0d61960a
teuthid <mariusz.przygodzki@gmail.com>
2019-03-13 23:51:28 +0100

    upload to master

--- test/test_eq_flash.h
+++ test/test_eq_flash.h
@@ -8,34 +8,33 @@
 test(EqFlashString) {
   EQ_FSTRING(strTest, "0123456789");
   EqFlashString __str(strTest);
-  uint8_t __length = __str.length();
-  assertEqual(__length, 10);
-  assertTrue(__str.compare("0123456789") == 0);
+  assertEqual(__str.length(), size_t(10));
+  assertEqual(__str.compare("0123456789"), 0);
   assertTrue(__str.compare("01234") > 0);
   assertTrue(__str.compare("1") < 0);
-  assertTrue(__str.compare_n("01234", 5) == 0);
-  assertTrue(__str.compare_n("01234", 6) != 0);
+  assertEqual(__str.compare_n("01234", 5), 0);
+  assertNotEqual(__str.compare_n("01234", 6), 0);
   char __s[25];
   __str.copy(__s);
-  assertTrue(__str.compare("0123456789") == 0);
+  assertEqual(__str.compare("0123456789"), 0);
   __str.copy(__s, 1);
-  assertTrue(strcmp(__s, "123456789") == 0);
+  assertEqual(strcmp(__s, "123456789"), 0);
   __str.copy_n(__s, 5);
-  assertTrue(strncmp(__s, "01234", 5) == 0);
+  assertEqual(strncmp(__s, "01234", 5), 0);
   __str.copy(__s);
   __str.append(__s);
-  assertTrue(strcmp(__s, "01234567890123456789") == 0);
+  assertEqual(strcmp(__s, "01234567890123456789"), 0);
   __str.copy(__s);
   __str.append_n(__s, 5);
-  assertTrue(strcmp(__s, "012345678901234") == 0);
-  assertTrue(strcmp(__str(__s), "0123456789") == 0);
+  assertEqual(strcmp(__s, "012345678901234"), 0);
+  assertEqual(strcmp(__str(__s), "0123456789"), 0);
   for (uint8_t __i = 0; __i < 10; __i++) {
-    assertTrue(__str.charAt(__i) == (char)(__i + 48));
-    assertTrue(__str[__i] == (char)(__i + 48));
+    assertEqual(__str.charAt(__i), char(__i + 48));
+    assertEqual(__str[__i], char(__i + 48));
   }
   aunit::fake::FakePrint fakePrint;
   fakePrint.print(__str);
-  assertTrue(strcmp("0123456789", fakePrint.getBuffer()));
+  assertTrue(strcmp("0123456789", fakePrint.getBuffer())); // ???
 }
 
 static const PROGMEM char *const strArrayTest[] = {"1", "22", "333", "4444",
@@ -44,7 +43,8 @@ static const PROGMEM char *const strArrayTest[] = {"1", "22", "333", "4444",
 test(EqFlashStringArray) {
   EqFlashStringArray<sizeof(strArrayTest) / sizeof(strArrayTest[0])> __strArray(
       strArrayTest);
-  assertTrue(__strArray.size() == 5);
-  assertTrue(__strArray[0].length() == 1);
-  assertTrue(__strArray[4].length() == 5);
+  assertEqual(__strArray.size(), size_t(5));
+  assertEqual(__strArray[0].length(), size_t(1));
+  assertEqual(__strArray[4].length(), size_t(5));
+  //assertEqual(__strArray[1].compare("22"), 0);
 }
